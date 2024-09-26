8ab1fd8d0a6b6e16aafd9590428903c16780c333
Elena <esowesp@epsg.upv.es>
2024-09-26 18:44:29 +0200

    setup acabado

--- EmitirBeacons/EmitirBeacons.ino
+++ EmitirBeacons/EmitirBeacons.ino
@@ -1,9 +1,42 @@
-void setup() {
-  // put your setup code here, to run once:
+//----------------------------------------------------
+// Código para emitir beacons desde SparkFun Pro nRF52840 Mini
+//----------------------------------------------------
+
+#include <bluefruit.h>
+
+
+// ----------------------------------------------------
+//                    SETUP()
+// ----------------------------------------------------
+void setup(){
+
+  Serial.begin(115200);
+  while(!Serial) delay(10);
+
+  Serial.println("-----------------------\n");
+  Serial.println(" PRUEBAS iBeacon  ");
+  Serial.println("-----------------------\n");
+
+  //Se inicializa el módulo Bluefruit:
+  Serial.println("Inicializando el módulo nRF52 Bluefruit");
+
+  Bluefruit.begin();
+
+  //Cambio de nombre del dispositivo:
+  Serial.println("Cambiando el nombre del dispositivo a GTI-Elena");
+
+  Bluefruit.setName("GTI-Elena");
+  Bluefruit.ScanResponse.addName();
+
+  Bluefruit.Advertising.start();
+
+  Serial.println("\nAdvertising");
+
+} //setup()
+
+void loop(){
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
-}
+
