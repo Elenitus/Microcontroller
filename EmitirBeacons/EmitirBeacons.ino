c944344b4dddf0226a54243ca44e2b314c6c1996
Elena <esowesp@epsg.upv.es>
2024-09-26 18:59:27 +0200

    funcion startAdvertising hecha

--- EmitirBeacons/EmitirBeacons.ino
+++ EmitirBeacons/EmitirBeacons.ino
@@ -28,11 +28,57 @@ void setup(){
   Bluefruit.setName("GTI-Elena");
   Bluefruit.ScanResponse.addName();
 
-  Bluefruit.Advertising.start();
+  startAdvertising();
 
   Serial.println("\nAdvertising");
 
 } //setup()
+
+
+// ----------------------------------------------------
+//            Función startAdvertising()
+// ----------------------------------------------------
+void startAdvertising(){
+  
+  Serial.println("startAdvertising()");
+
+  Serial.println("Bluefruit.Advertising.stop()");
+  Bluefruit.Advertising.stop();
+
+  //Paquete de advertising:
+  Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
+  Bluefruit.Advertising.addTxPower();
+
+  //Incluir nombre:
+  Serial.println("Bluefruit.Advertising.addName()");
+  Bluefruit.Advertising.addName();
+
+  Serial.println("Bluefruit.Advertising.setBeacon( elBeacon )");
+  
+  uint8_t beaconUUID[16] = {
+    'E', 'P', 'S', 'G', '-', 'G', 'T', 'I',
+    '-', 'P', 'R', 'O', 'Y', '-', '3', 'A'
+  };
+
+  BLEBeacon elBeacon (beaconUUID, 12, 34, 73 );
+  elBeacon.setManufacturer(0x004c); //id de Apple
+  Bluefruit.Advertising.setBeacon(elBeacon);
+
+  Bluefruit.Advertising.restartOnDisconnect(true);
+  Bluefruit.Advertising.setInterval(32, 244); //en unidades de 0.625ms
+  Bluefruit.Advertising.setFastTimeout(30); //número de segundos en modo rápido
+  Bluefruit.Advertising.start(0); // 0 = no pares el advertising después de n segundos.
+
+  Serial.println("Bluefruit.Advertising.start(0)");
+
+}//startAdvertising()
+
+
+
+
+   
+
+
 
 void loop(){
 
