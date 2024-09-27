ed008d1e0c18310b5ed6cddfbecc48d65d7be286
Elena <esowesp@epsg.upv.es>
2024-09-27 11:12:40 +0200

    archivo mediciones vacio hecho

---
+++ mediciones/Mediciones/Mediciones.ino
@@ -0,0 +1,14 @@
+// Definicion de los pines UART
+#define RX_PIN 15
+#define TX_PIN 17
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
